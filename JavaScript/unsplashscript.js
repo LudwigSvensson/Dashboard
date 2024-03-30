c99b5e45a95a1a22de0e724e4c70dee159a7a5eb
LudwigSvensson <ludwig.gifrod@hotmail.com>
2024-03-30 20:43:18 +0100

    Fixat Randomisen så den fungerar helt

--- JavaScript/unsplashscript.js
+++ JavaScript/unsplashscript.js
@@ -1,4 +1,4 @@
-let clientID ="-1P52t6jSbqITlH1Q9pcsJX8WCXziJ3ysoI2N6P4eMw";
+let clientID ="9GVRMIgLPj9yB1D5FIHLeDOlkEveXprrsgKJ2mupnAw";
 let endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}`;
 
 function randomBackground(){
@@ -13,15 +13,3 @@ fetch(endpoint)
 const button = document.getElementById("randomBG");
 button.addEventListener("click", randomBackground);
 
-/*function setRandomBackground() {
-    fetch("https://api.unsplash.com/photos/random?client_id=cpKOFdn1hyrqMDJK0dg8hlB_rOlyUcIRiUyjc9DCgLE")
-      .then((response) => response.json())
-      .then((data) => {
-        const imageUrl = data.urls.regular;
-        document.body.style.backgroundImage = `url(${imageUrl})`;
-      })
-      .catch((error) => console.error("Error fetching random image:", error));
-  }
-  
-  const button = document.getElementById("randomBG");
-  button.addEventListener("click", setRandomBackground);*/
